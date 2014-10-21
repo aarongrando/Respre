9702ebd46a4c3babe825bb7bf80e3c02f3ee9f23
Aaron Grando <aaron.grando@gmail.com>
2014-10-21 10:35:12 -0400

    read folders within 'preview' as pages, build nav to move between pages – needs style work

--- index.php
+++ index.php
@@ -1,38 +1,61 @@
 <?
 	
-	$directory 	= 'preview';
-	$files		= scandir($directory);
-	sort($files, SORT_NUMERIC);
+	// Directory listing for 'preview' directory – used to generate page nav
+	$preview_directory 	= 'preview';
+	$preview_files = scandir($preview_directory);
 
+	// Directory listing for the target preview folder. 
+	// If the '?page=' query string is unset, this is the same as the preview directory above. 
+	if (isset($_GET['page'])) {
+		$display_directory = 'preview/' . $_GET['page'];
+		$display_files = scandir($display_directory);
+	} else {
+		$display_directory = $preview_directory;
+		$display_files = $preview_files;
+	}
 
-	$markup = '';
-	$style  = '';
+	sort($preview_files, SORT_NUMERIC);
+	sort($display_files, SORT_NUMERIC);
 
-	foreach($files as &$file) {
+	$preview_markup = '';
+	$preview_style  = '';
+	$nav_markup = '';
+
+	// Loop through the top level of files and build the navigation.
+	foreach($preview_files as &$file) {
+		$resolution = explode(".", $file)[0];
+		if (intval($resolution) == 0 && strpos($file, '.') === FALSE) {
+			$nav_markup .= '<a href="/?page='.$file.'">'.$file.'</a>';
+		}
+		unset($resolution);
+	}
+
+	// Loop through the target preview folder and build the markup for the comp previews.
+	foreach($display_files as &$file) {
 		
-		$file_position  = array_search($file, $files);
-
-		if($file_position != 0) {
-			$lower_file = $files[$file_position-1];
-			$lower_resolution = explode(".", $lower_file)[0];
-		}
-
-		if($file_position+1 != count($files)) {
-			$upper_file = $files[$file_position+1];
-			$upper_resolution = explode(".", $upper_file)[0];
-		}
-
 		$resolution = explode(".", $file)[0];
 
 		if(intval($resolution) > 0) {
-			$markup .= '<img src="preview/' . $file . '" class="res_' . $resolution . '" />';
+			$file_position  = array_search($file, $display_files);
+
+			if($file_position != 0) {
+				$lower_file = $display_files[$file_position-1];
+				$lower_resolution = explode(".", $lower_file)[0];
+			}
+
+			if($file_position+1 != count($display_files)) {
+				$upper_file = $display_files[$file_position+1];
+				$upper_resolution = explode(".", $upper_file)[0];
+			}
+
+			$preview_markup .= '<img src="'.$display_directory.'/' . $file . '" class="res_' . $resolution . '" />';
 			
 			if (isset($upper_resolution)) {
 				$media_upper_limit = 'and (max-width: '.$upper_resolution.'px)';
 			} else {
 				$media_upper_limit = '';
 			}
-			$style .= 
+			$preview_style .= 
 				'@media (min-width: '.$resolution.'px) '. $media_upper_limit .' {
 					#respre img.res_'.$resolution.' {
 						display: block;
@@ -48,8 +71,8 @@
 		unset($resolution);
 		unset($media_upper_limit);
 	}
-
 ?>
+
 
 <!DOCTYPE html>
 <html>
@@ -64,13 +87,20 @@
 			display: none;
 			width: 100%;
 		}
-		<? echo $style ?>
+		<? echo $preview_style ?>
 	</style>
 </head>
 
 <body>
+	<? if(strlen($nav_markup) > 0) { ?>
+		<div id="nav">
+			<a href="/">Index</a>
+			<? echo $nav_markup ?>
+		</div>
+	<? } ?>
+
 	<div id="respre">
-		<? echo $markup ?>
+		<? echo $preview_markup ?>
 	</div>
 </body>
 </html>
